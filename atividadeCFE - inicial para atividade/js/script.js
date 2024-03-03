ee5fbdbb9e2797b3202a28385c9cee90c244fef9
Bruno Gomes <146124099+brunogomesz-c@users.noreply.github.com>
2024-03-03 14:44:27 -0300

    Add files via upload

---
+++ atividadeCFE - inicial para atividade/js/script.js
@@ -0,0 +1,26 @@
+//efeito do botão voltar ao Topo
+
+function topo () {
+    window.scrollTo;
+{
+    top: 0,
+    letf: 0,
+    behavior: 'smooth'
+}
+}
+
+//Validação de Login
+
+function Login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == "admin" && senha == "123456"){
+        window.location.href = "index.html";
+        logado = 1;
+    }
+    if(logado == 0){
+        alert("Usuário ou senha incorretos");  
+}
+//Ativar alert no botão cadastrar
